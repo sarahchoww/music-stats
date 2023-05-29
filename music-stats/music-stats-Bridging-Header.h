41c54ceae2e12d20e674b4239b4452f2e3097508
sarahchow <chow.sarah@hotmail.com>
2023-05-29 17:26:34 -0400

    took from existing tutorial

---
+++ music-stats/music-stats-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+// Bring all spotify headers into project, bridges it over
+
+#import <SpotifyiOS/SpotifyiOS.h>
+
